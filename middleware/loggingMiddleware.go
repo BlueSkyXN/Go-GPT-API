fb931c0cf61d0076cc70fc6234ce61d158a8e7ac
BlueSkyXN <63384277+BlueSkyXN@users.noreply.github.com>
2023-05-31 17:54:07 +0800

    0.3

--- middleware/loggingMiddleware.go
+++ middleware/loggingMiddleware.go
@@ -16,7 +16,6 @@ var (
 )
 
 func init() {
-	// Initialize the logger
 	file, err := os.OpenFile("app.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		log.Fatalf("error opening file: %v", err)
@@ -24,7 +23,6 @@ func init() {
 
 	logger = log.New(file, "", log.LstdFlags)
 
-	// Initialize Gin
 	gin.ForceConsoleColor()
 	gin.SetMode(gin.ReleaseMode)
 }
@@ -41,62 +39,53 @@ func (w responseWriter) Write(b []byte) (int, error) {
 
 func LoggingMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-
-		// Read the request body
 		bodyBytes, err := io.ReadAll(c.Request.Body)
 		if err != nil {
 			logger.Printf("Error reading body: %v", err)
 			return
 		}
 
-		// Restore the body to its original state for the next middleware
 		c.Request.Body = io.NopCloser(bytes.NewBuffer(bodyBytes))
 
-		// Get the request details
 		ip := c.ClientIP()
 		startTime := time.Now()
 		method := c.Request.Method
 		path := c.Request.URL.Path
 
-		// Log the request details
 		logger.Printf("Request details: IP: %s, Start Time: %s, Method: %s, Path: %s, Request Body: %s",
 			ip, startTime.Format(time.RFC1123), method, path, string(bodyBytes))
 
-		// Log the request headers
 		for name, values := range c.Request.Header {
-			// Loop over all values for the name.
 			for _, value := range values {
 				logger.Printf("Request header: %s: %s\n", name, value)
 			}
 		}
 
-		// Create our custom response writer
 		writer := &responseWriter{body: bytes.NewBufferString(""), ResponseWriter: c.Writer}
 
-		// Replace the original response writer with our custom one
 		c.Writer = writer
 
-		// Process the request
 		c.Next()
 
-		// Get the response details
 		statusCode := c.Writer.Status()
 
-		// Calculate response time
 		latency := time.Since(startTime)
 
-		// Only log the response details if the response body does not contain "status": "in_progress"
-		if !strings.Contains(writer.body.String(), "\"status\": \"in_progress\"") {
-			// Log the response details
-			logger.Printf("Response details: Status Code: %d, Latency: %v, Response Body: %s",
-				statusCode, latency, writer.body.String())
+		filteredBody := &bytes.Buffer{}
 
-			// Log the response headers
-			for name, values := range writer.Header() {
-				// Loop over all values for the name.
-				for _, value := range values {
-					logger.Printf("Response header: %s: %s\n", name, value)
-				}
+		lines := strings.Split(writer.body.String(), "\n")
+		for _, line := range lines {
+			if strings.Contains(line, `"status": "finished_successfully"`) {
+				filteredBody.WriteString(line + "\n")
+			}
+		}
+
+		logger.Printf("Response details: Status Code: %d, Latency: %v, Response Body: %s",
+			statusCode, latency, filteredBody.String())
+
+		for name, values := range writer.Header() {
+			for _, value := range values {
+				logger.Printf("Response header: %s: %s\n", name, value)
 			}
 		}
 	}
